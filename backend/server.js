2deec21e8fab7cb3854b5519e7e00d603cf0dc69
MNVallone <maniva04@gmail.com>
2025-02-11 21:15:34 -0300

    fetch a TMDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
  
@@ -10,6 +13,9 @@ const PORT = ENV_VARS.PORT
 app.use(express.json()); // parsear req.body a un JSON
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
+
 
 app.listen(PORT, () => {
 console.log("server started at http://localhost:" + PORT);
