3fa625968704fc64b0867a8baf84e019f2f13998
MNVallone <maniva04@gmail.com>
2025-02-12 19:34:53 -0300

    AuthScreen y Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
+import Footer from "./components/Footer";
 
 function App() {
  
   return (
-
+<>
     <Routes>
       <Route path="/" element={<HomePage/>}> </Route>
       <Route path="/login" element={<LoginPage/>}> </Route>
       <Route path="/signup" element={<SignUpPage/>}> </Route>
     </Routes>
-
+   <Footer />
+</>
   );
 
 }
