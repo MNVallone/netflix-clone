52f422658e66380615cb63a8be02d146538118fc
MNVallone <maniva04@gmail.com>
2025-02-15 18:15:34 -0300

    History Page y Not Found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
 import WatchPage from "./pages/WatchPage"
 import SearchPage from "./pages/SearchPage"
+import HistoryPage from "./pages/HistoryPage"
+import NotFoundPage from './pages/404';
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
@@ -33,7 +35,9 @@ function App() {
       <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
       <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
       <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
-      <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/"} />} />
+      <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+      <Route path="/history" element={user ? <HistoryPage /> : <Navigate to={"/login"} />} />
+      <Route path="/*" element={<NotFoundPage />} />
 
     </Routes>
    <Footer />
