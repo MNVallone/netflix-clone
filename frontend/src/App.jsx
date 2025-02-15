e85a097ad76f535844abc01eb27c823036608b37
MNVallone <maniva04@gmail.com>
2025-02-15 16:40:47 -0300

    Watch Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
+import WatchPage from "./pages/WatchPage"
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
@@ -30,6 +31,7 @@ function App() {
       <Route path="/" element={<HomePage/>}> </Route>
       <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
       <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+      <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
     </Routes>
    <Footer />
 
